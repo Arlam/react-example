623f8f9840d9469c5a337dd157a59db048cf024c
Andrii Arlamovskyi <aarlam@softserveinc.com>
2016-11-28 17:53:53 +0200

    initial commit

---
+++ src/client/app/main.js
@@ -0,0 +1,6 @@
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App.jsx';
+
+render(<App/>, document.getElementById('app'));
